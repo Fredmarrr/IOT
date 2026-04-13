cd62f6a713c3e84bea95d5aeaa9f201b3f544caa
Fredmarrr <fredygarcia31512yahoo.es@gmail.com>
2026-04-12 23:28:27 -0600

    Update MenuCasaInteligente.cs

--- Gestion de dispositivos en iot en una casa inteligente/MenuCasaInteligente.cs
+++ Gestion de dispositivos en iot en una casa inteligente/MenuCasaInteligente.cs
@@ -1,1 +1,58 @@
-﻿
+public class MenuCasaInteligente
+{
+    List<DispositivoBase> _dispositivos;
+    public MenuCasaInteligente(List<DispositivoBase> dispositivos)
+    {
+        _dispositivos = dispositivos;
+    }
+    public void Menu()
+    {
+        bool salir = false;
+        int num;
+        Console.Clear();
+        Console.WriteLine("=== PANEL CENTRAL DE LA CASA INTELIGENTE ===");
+
+        // Imprimimos todos los dispositivos disponibles
+        for (int i = 0; i < _dispositivos.Count; i++)
+        {
+            var disp = _dispositivos[i];
+            string estado = disp.EstadoOF ? "ON" : "OFF";
+            Console.WriteLine($"{i + 1}. {disp.NombreDisp} [{estado}]");
+        }
+        Console.Write("\nElige el número del dispositivo a controlar: ");
+        Console.WriteLine("0. Salir del sistema");
+        do
+        {
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num == 0)
+                {
+                    salir = true;
+                    continue;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Dato invalido");
+                num = -1;
+            }
+        } while (num == -1);
+        if(num>0&&num<_dispositivos.Count)
+        {
+            DispositivoBase dispositivo = _dispositivos[num-1];
+            MenuCasa(dispositivo);
+        }
+
+    }
+    private void MenuCasa(DispositivoBase dispositivo)
+    {
+        Console.WriteLine($"\n--- Controlando: {dispositivo.NombreDisp} ---");
+        Console.WriteLine("1. Encender / Apagar");
+        Console.WriteLine("2. Activar Modo Ecológico");
+        if(dispositivo is Televisor)
+        {
+
+        }
+    }
+}﻿
