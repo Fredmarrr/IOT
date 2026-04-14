72d973a9528d15a3dd1935cf01be74b35e7f9fff
Fredmarrr <fredygarcia31512yahoo.es@gmail.com>
2026-04-14 12:53:46 -0600

    Update Program.cs

--- Gestion de dispositivos en iot en una casa inteligente/Program.cs
+++ Gestion de dispositivos en iot en una casa inteligente/Program.cs
@@ -1,2 +1,7 @@
-﻿Televisor tele= new Televisor(5,"Televisor_Sala","002",14);
-tele.Encender();
+List<DispositivoBase> menu = new List<DispositivoBase>;
+MenuCasaInteligente panel=new MenuCasaInteligente(menu);
+menu.Add(new Televisor(20, "Tele1", "003", 40));
+menu.Add(new AireAcondicionado(2, "AireSala", "005", 23));
+menu.Add(new PuertaInteligente(true, false, "PuertaFrente", "008"));
+menu.Add(new Radio(23, "RadioDorm", "001", 23.5m));
+panel.Menu();
